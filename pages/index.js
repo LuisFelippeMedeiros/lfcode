39662d04b84e1d87435ff07594412784820f5e9e
Luis Felippe Pereira de Medeiros <lfelippemedeiros@gmail.com>
2022-08-31 09:21:55 -0400

    Estilizando site

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,19 @@ export default function Home() {
         <title>LF Code</title>
         <meta name="description" content="LF Code LTDA" />
         <link rel="icon" href="/favicon.ico" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
 
-      <main className={styles.logo}>
-        <Image src="/sem-fundo.png" alt="LF Code Logo" width={142} height={131} />
-      </main>
+        <main className={styles.container}>
+          <Image src="/lfcode.svg" alt="LF Code Logo" width={142} height={131} />
+        </main>
 
+    <div className={styles.whatsapp}>
+      <a href="https://wa.me/5565981106000?text=Olá Luís, vim do seu site e gostaria de mais informações." target="_blank" rel="noreferrer">
+        <Image src="/whatsapp.svg" alt="Fale conosco através do whatsapp" width={60} height={60} />
+      </a>
+    </div>
     </div>
   )
 }
